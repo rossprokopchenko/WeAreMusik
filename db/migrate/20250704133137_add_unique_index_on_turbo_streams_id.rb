249ca7bfc25f52830f923af13f956be89feeedf4
DESKTOP-39GP9U3\Rostiku <rosskakashka@gmail.com>
2025-07-08 16:49:07 -0300

    removed unneccesary migrations

--- db/migrate/20250704133137_add_unique_index_on_turbo_streams_id.rb
+++
@@ -1,7 +0,0 @@
-class AddUniqueIndexOnTurboStreamsId < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
-  def change
-    add_index :turbo_streams, :id, unique: true, algorithm: :concurrently
-  end
-end
